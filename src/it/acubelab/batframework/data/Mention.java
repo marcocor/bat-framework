960e0b26f2f94d3fd47f58dca88f9125b0e3d039
marcocor <cornolti@di.unipi.it>
2014-04-02 17:15:29 +0200

    Added compare method

--- src/it/acubelab/batframework/data/Mention.java
+++ src/it/acubelab/batframework/data/Mention.java
@@ -2,17 +2,18 @@ package it.acubelab.batframework.data;
 
 import java.io.Serializable;
 
-public class Mention implements Serializable, Cloneable {
+public class Mention implements Serializable, Cloneable, Comparable<Mention> {
 	private static final long serialVersionUID = 1L;
-	private int position; //starting position of the annotation in the original text
-	private int length; //length of the annotation in the original text
+	private int position; // starting position of the annotation in the original
+							// text
+	private int length; // length of the annotation in the original text
 
-	public Mention (int position, int length){
+	public Mention(int position, int length) {
 		this.position = position;
 		this.length = length;
 	}
-	
-	public int getPosition(){
+
+	public int getPosition() {
 		return position;
 	}
 
@@ -20,33 +21,37 @@ public class Mention implements Serializable, Cloneable {
 		return length;
 	}
 
-	@Override public boolean equals(Object m){
+	@Override
+	public boolean equals(Object m) {
 		Mention men = (Mention) m;
 		return (this.position == men.position && this.length == men.length);
 	}
 
-	@Override public int hashCode() {
-		return (""+position+length).hashCode();
+	@Override
+	public int hashCode() {
+		return ("" + position + length).hashCode();
 	}
-	
-	@Override public Object clone(){
+
+	@Override
+	public Object clone() {
 		return new Mention(this.getPosition(), this.getLength());
 	}
 
 	public boolean overlaps(Mention m) {
 		int p1 = this.getPosition();
-		int l1 = this.getLength();		
-		int e1 = p1+l1-1;
+		int l1 = this.getLength();
+		int e1 = p1 + l1 - 1;
 		int p2 = m.getPosition();
 		int l2 = m.getLength();
-		int e2 = p2+l2-1;
-		return (  (p1 <= p2 &&  p2 <= e1)
-				|| (p1 <= e2 &&  e2 <= e1)
-				|| (p2 <= p1 &&  p1 <= e2)
-				|| (p2 <= e1 &&  e1 <= e2)
-				);
+		int e2 = p2 + l2 - 1;
+		return ((p1 <= p2 && p2 <= e1) || (p1 <= e2 && e2 <= e1)
+				|| (p2 <= p1 && p1 <= e2) || (p2 <= e1 && e1 <= e2));
 
 	}
 
+	@Override
+	public int compareTo(Mention m) {
+		return this.getPosition() - m.getPosition();
+	}
 
 }
