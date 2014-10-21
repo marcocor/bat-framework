449b864bf2ceb798c1d05eb2635c05546317479d
marcocor <cornolti@di.unipi.it>
2014-10-21 13:14:25 +0200

    Added match relation to strongly check an annotation's mention

---
+++ src/it/acubelab/batframework/metrics/StrongMentionAnnotationMatch.java
@@ -0,0 +1,32 @@
+package it.acubelab.batframework.metrics;
+
+import it.acubelab.batframework.data.Annotation;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Vector;
+
+public class StrongMentionAnnotationMatch implements MatchRelation<Annotation>{
+	@Override
+	public boolean match(Annotation t1, Annotation t2) {
+			return t1.getLength() == t2.getLength() && t1.getPosition() == t1.getPosition();
+	}
+
+	@Override
+	public List<HashSet<Annotation>> preProcessOutput(List<HashSet<Annotation>> computedOutput) {
+		List<HashSet<Annotation>> nonOverlappingOutput = new Vector<HashSet<Annotation>>();
+		for (HashSet<Annotation> s: computedOutput)
+			nonOverlappingOutput.add(Annotation.deleteOverlappingAnnotations(s));
+		return nonOverlappingOutput;
+	}
+
+	@Override
+	public List<HashSet<Annotation>> preProcessGoldStandard(List<HashSet<Annotation>> goldStandard) {
+		return preProcessOutput(goldStandard);
+	}
+
+	@Override
+	public String getName() {
+		return "Strong mention annotation match";
+	}
+}
