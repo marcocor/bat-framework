fea81a3752e0362be42660af5a1140a71aaf75eb
marcocor <cornolti@di.unipi.it>
2014-10-21 17:19:06 +0200

    added a CLI script to test the output of an annotator in CSV format against a dataset in CSV format.

---
+++ src/it/acubelab/batframework/datasetPlugins/CsvDataset.java
@@ -0,0 +1,109 @@
+package it.acubelab.batframework.datasetPlugins;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Vector;
+
+import it.acubelab.batframework.data.Annotation;
+import it.acubelab.batframework.data.Mention;
+import it.acubelab.batframework.data.ScoredAnnotation;
+import it.acubelab.batframework.data.Tag;
+import it.acubelab.batframework.problems.A2WDataset;
+import it.acubelab.batframework.utils.AnnotationException;
+import it.acubelab.batframework.utils.Pair;
+import it.acubelab.batframework.utils.ProblemReduction;
+
+public class CsvDataset implements A2WDataset {
+	String name;
+	List<String> docIds;
+	List<HashSet<Annotation>> gold;
+
+	public CsvDataset(String filename, String name)
+			throws NumberFormatException, AnnotationException, IOException {
+		this.name = name;
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				new FileInputStream(filename)));
+		String line = null;
+		HashMap<String, HashSet<Annotation>> dsHm = new HashMap<>();
+		while ((line = br.readLine()) != null) {
+			String[] tokens = line.split(",");
+			if (tokens.length != 4){
+				br.close();
+				throw new RuntimeException(String.format(
+						"Line in file %s malformed: [%s]", filename, line));
+			}
+			String docId = tokens[0];
+			int start = Integer.parseInt(tokens[1]);
+			int end = Integer.parseInt(tokens[2]);
+			int wikiId = Integer.parseInt(tokens[3]);
+			if (start < 0 || end < 0 || wikiId < 0){
+				br.close();
+				throw new RuntimeException(
+						"start, end and wikipediaId must be greater that zero.");
+			}
+			if (!dsHm.containsKey(docId))
+				dsHm.put(docId, new HashSet<Annotation>());
+			dsHm.get(docId).add(
+					new Annotation(start, end - start, wikiId));
+		}
+		br.close();
+		
+		docIds = new Vector<>(dsHm.keySet());
+		Collections.sort(docIds);
+		
+		gold = new Vector<>();
+		for (String docId : docIds)
+			gold.add(dsHm.get(docId));
+	}
+
+	@Override
+	public int getSize() {
+		return docIds.size();
+	}
+
+	@Override
+	public int getTagsCount() {
+		int count = 0;
+		for (HashSet<Annotation> s: gold)
+			count += s.size();
+		return count;
+	}
+
+	@Override
+	public List<HashSet<Tag>> getC2WGoldStandardList() {
+		return ProblemReduction.A2WToC2WList(this.getA2WGoldStandardList());
+	}
+	
+	@Override
+	public List<HashSet<Annotation>> getD2WGoldStandardList() {
+		return getA2WGoldStandardList();
+	}
+
+	@Override
+	public List<String> getTextInstanceList() {
+		return this.docIds;
+	}
+	
+	@Override
+	public List<HashSet<Mention>> getMentionsInstanceList() {
+		return ProblemReduction.A2WToD2WMentionsInstance(getA2WGoldStandardList());
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public List<HashSet<Annotation>> getA2WGoldStandardList() {
+		return gold;
+	}
+
+}
---
+++ src/it/acubelab/batframework/systemPlugins/MockAnnotator.java
@@ -0,0 +1,64 @@
+package it.acubelab.batframework.systemPlugins;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import it.acubelab.batframework.data.Annotation;
+import it.acubelab.batframework.data.Mention;
+import it.acubelab.batframework.data.ScoredAnnotation;
+import it.acubelab.batframework.data.ScoredTag;
+import it.acubelab.batframework.data.Tag;
+import it.acubelab.batframework.problems.Sa2WSystem;
+import it.acubelab.batframework.utils.AnnotationException;
+import it.acubelab.batframework.utils.ProblemReduction;
+
+public class MockAnnotator implements Sa2WSystem {
+
+	private HashMap<String, HashSet<ScoredAnnotation>> answers;
+	private String name;
+
+	public MockAnnotator(HashMap<String, HashSet<ScoredAnnotation>> answers,
+			String name) {
+		this.answers = answers;
+		this.name = name;
+	}
+
+	@Override
+	public HashSet<Annotation> solveA2W(String text) throws AnnotationException {
+		return ProblemReduction.Sa2WToA2W(answers.get(text));
+	}
+
+	@Override
+	public HashSet<Tag> solveC2W(String text) throws AnnotationException {
+		return ProblemReduction.A2WToC2W(solveA2W(text));
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public long getLastAnnotationTime() {
+		return 0;
+	}
+
+	@Override
+	public HashSet<Annotation> solveD2W(String text, HashSet<Mention> mentions)
+			throws AnnotationException {
+		return ProblemReduction.Sa2WToD2W(solveSa2W(text), mentions,
+				Float.NEGATIVE_INFINITY);
+	}
+
+	@Override
+	public HashSet<ScoredTag> solveSc2W(String text) throws AnnotationException {
+		return ProblemReduction.Sa2WToSc2W(solveSa2W(text));
+	}
+
+	@Override
+	public HashSet<ScoredAnnotation> solveSa2W(String text)
+			throws AnnotationException {
+		return answers.get(text);
+	}
+
+}
